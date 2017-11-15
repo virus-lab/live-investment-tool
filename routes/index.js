63c401eaa2ef987784624e50bafefc1d6fe1dd59
PJunhyuk-mac <jgravity@naver.com>
2017-11-15 22:42:33 +0900

    Generalize codes

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'X2O공모전 모의 투자 시스템' });
+  res.render('pages/index', { title: '실시간 모의 투자 시스템' });
 });
 
 /* make route to invest page */
 router.get('/invest', function(req, res, next) {
-  res.render('pages/invest', { title: 'X2O공모전 모의 투자 시스템' });
+  res.render('pages/invest', { title: '실시간 모의 투자 시스템' });
 });
 
 /* make route to invest-list page */
 router.get('/invest/list', function(req, res, next) {
-  res.render('pages/invest-list', { title: 'X2O공모전 모의 투자 시스템' });
+  res.render('pages/invest-list', { title: '실시간 모의 투자 시스템' });
 });
 
 /* make route to result page */
 router.get('/invest/result', function(req, res, next) {
-  res.render('pages/invest-result', { title: 'X2O공모전 모의 투자 시스템' });
+  res.render('pages/invest-result', { title: '실시간 모의 투자 시스템' });
 });
 
 module.exports = router;
